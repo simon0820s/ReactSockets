c3b6349eb3882b285440e87ec6a7ae766245c325
simon0820s <simon0820s@gmail.com>
2023-08-02 22:48:09 -0500

    style chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
 
   const [message, setMessage] = useState("")
   const [messages, setMessages] = useState([])
-  
+
   const handleSubmit = (e) => {
     e.preventDefault()
 
@@ -31,21 +31,27 @@ export default function App() {
   const receiveMessage = (message) => setMessages((state) => [...state, message])
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className='h-screen bg-zinc-800 text-neutral-100 flex items-center justify-center'>
+      <form onSubmit={handleSubmit} className='bg-zinc-900 p-10 flex flex-col gap-2 rounded-xl'>
+        <h1 className='text-2xl font-bold ml-1'>React Chat</h1>
         <input type='text' placeholder='Write your message ...'
-          onChange={(e) => setMessage(e.target.value)} />
-        <button type='submit'>
+          onChange={(e) => setMessage(e.target.value)}
+          className='border-none rounded-md border-zinc-500 p-2 w-full bg-zinc-800 text-neutral-100 outline-none focus:shadow-teal-800 shadow-md transition-all ease-in-out duration-500'/>
+        
+        <ul>
+          {messages.map((message, i) => (
+            <li key={i} className={
+              `my-2 p-2 table text-md font-medium rounded-md ${message.from === 'Me' ? 'bg-sky-950 ml-auto': 'bg-sky-700'}`
+            }>
+              <span className='text-xs font-medium text-teal-300 block'>{message.from}</span><span className='text-md'>{message.body}</span>
+            </li>
+          ))}
+        </ul>
+
+        <button className='py-2 px-4 w-1/3 bg-teal-800 hover:bg-teal-900 hover:shadow-teal-400 shadow-lg font-bold rounded-md transition-all ease-in-out duration-500' type='submit'>
           Send
         </button>
       </form>
-      <ul>
-        {messages.map((message, i) => (
-          <li key={i}>
-            {message.from}:{message.body}
-          </li>
-        ))}
-      </ul>
     </div>
   )
 }
